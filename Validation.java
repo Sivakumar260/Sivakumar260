5ce37de9932cc8f1a3cdcad002a2f9d6f7b258b1
Sivakumar260 <62737107+Sivakumar260@users.noreply.github.com>
2022-02-01 15:13:30 +0530

    Add files via upload

---
+++ Validation.java
@@ -0,0 +1,52 @@
+package Assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Validation {
+    
+	String email;
+	
+	void validation(String email) {
+		this.email=email;
+	  }
+	
+	public void isvalid(boolean email) {
+		if(email) {
+		     System.out.println("User Authenticated");
+	    }
+		else {
+			 System.out.println("Invalid User");
+		}
+	}
+		
+	public static void main(String[] args) {
+		
+		ArrayList<String> a = new ArrayList<String>();
+		Validation obj = new Validation();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		a.add("siva@gmail.com");
+		a.add("kumar@gmail.com");
+		a.add("yuva@gmail.com");
+		a.add("vicky@gmail.com");
+		a.add("rishi@gmail.com");
+		a.add("saravana@gmail.com");
+		a.add("sakthi@gmail.com");
+		a.add("prakash@gmail.com");
+		a.add("arun@gmail.com");
+		a.add("swetha@gmail.com");
+		
+		
+		System.out.println("Enter the email id : ");
+		String email = sc.next();
+		
+		boolean b = a.contains(email);
+		obj.isvalid(b);
+		
+	}
+}
+
